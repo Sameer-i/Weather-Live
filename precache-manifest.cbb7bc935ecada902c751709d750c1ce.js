4d1480c9d5c8be209af6ae42bbbc9c0e85651c6b
Sameer-i <sameershaik2512@gmail.com>
2023-04-25 10:31:32 +0530

    Updates

---
+++ precache-manifest.cbb7bc935ecada902c751709d750c1ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea5668d4cb69b617d256ee89e3ca232",
+    "url": "/Weather-Live/index.html"
+  },
+  {
+    "revision": "a27ff427b2d1568154b2",
+    "url": "/Weather-Live/static/css/main.c7924132.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/Weather-Live/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Live/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27ff427b2d1568154b2",
+    "url": "/Weather-Live/static/js/main.b79904fd.chunk.js"
+  },
+  {
+    "revision": "be3f5c53f07ba50793a8",
+    "url": "/Weather-Live/static/js/runtime-main.ace13ebe.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Weather-Live/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "060aacee43229cc133e1c0ec956b670d",
+    "url": "/Weather-Live/static/media/background.060aacee.jpg"
+  },
+  {
+    "revision": "62c7a47b253f3108f95bdbe9883fc35f",
+    "url": "/Weather-Live/static/media/city.62c7a47b.jpg"
+  }
+]);
